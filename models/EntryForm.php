ac590a62e1f2c0e563c687c3826d9b2035c69f9e
hudenko <hudenko@inventive.ru>
2018-08-03 15:05:24 +0300

    New form

---
+++ models/EntryForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hudenko
+ * Date: 03.08.2018
+ * Time: 14:35
+ */
+
+namespace app\models;
+
+
+class EntryForm
+{
+
+}
---
+++ views/site/entry.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hudenko
+ * Date: 03.08.2018
+ * Time: 15:00
+ */
