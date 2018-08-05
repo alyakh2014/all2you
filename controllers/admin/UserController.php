5247fa9a12da653f3c3bad32c804d78d3fd04276
Alya <alyakh2014@gmail.com>
2018-08-05 09:08:06 +0300

    Make admin page

---
+++ controllers/admin/UserController.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\controllers\admin;
+use yii\web\Controller;
+/**
+ * Created by PhpStorm.
+ * User: A.Khudenko
+ * Date: 05.08.2018
+ * Time: 09:04
+ */
+class UserController extends Controller{
+    public function actionIndex(){
+        return "Admin";
+    }
+
+
+}
