de8e70de96a416a2e8b31d42113d943d25e61f2b
hudenko <hudenko@inventive.ru>
2018-08-06 15:18:45 +0300

    Work with database

---
+++ models/Country.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hudenko
+ * Date: 06.08.2018
+ * Time: 15:08
+ */
+
+namespace app\models;
+use yii\db\ActiveRecords;
+
+class Country extends ActiveRecords
+{
+
+}
