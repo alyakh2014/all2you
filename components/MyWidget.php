8ca97d75581c6445bdf58060ae4097f81a1be226
hudenko <hudenko@inventive.ru>
2018-08-21 13:18:28 +0300

    Widget

--- components/MyWidget.php
+++ components/MyWidget.php
@@ -22,6 +22,8 @@ class MyWidget extends Widget
 
     public function run(){
         $content = ob_get_clean();
+        $content = strtoupper($content, 'utf-8');
        // return $this->render('my', ['name'=>$this->name]);
+        return $this->render("my", compact('content'));
     }
 }
