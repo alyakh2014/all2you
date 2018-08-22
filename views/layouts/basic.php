3dea1661dd01d55ce349854a08571cceb1695303
hudenko <hudenko@inventive.ru>
2018-08-22 09:27:49 +0300

    views/layouts/basic.php

--- views/layouts/basic.php
+++ views/layouts/basic.php
@@ -20,7 +20,7 @@ AppAsset::register($this);
 <div class="wrap">
     <div class="container">
         <ul class="nav nav-pills">
-            <li role="presentation" class="active"><?=Html::a('Главная', '/web/')?></li>
+            <li role="presentation" class="active"><?=Html::a('Главная', '/')?></li>
             <li role="presentation" class="active"><?=Html::a('Статьи', ['post/index'])?></li>
             <li role="presentation" class="active"><?=Html::a('Статья', ['post/show'])?></li>
         </ul>
