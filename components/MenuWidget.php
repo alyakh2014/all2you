a658e6e0df55fdd1b19c8ae2df192a7aea40aab9
Alya <alyakh2014@gmail.com>
2018-08-26 09:10:16 +0300

    menu widget

---
+++ components/MenuWidget.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A.Khudenko
+ * Date: 26.08.2018
+ * Time: 09:07
+ */
+
+namespace app\components;
+use yii\jui\Widget;
+
+class MenuWidget extends Widget
+{
+
+    function init(){
+
+    }
+
+    public function run(){
+        return 'Menu';
+    }
+}
